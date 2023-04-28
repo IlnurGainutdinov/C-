de1b1a947efcb9a48aa9042251fe85cc45cb3d7f
Ilnur <gainutdinov86@list.ru>
2023-04-28 16:25:22 +0400

    Added Task

---
+++ Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// if(number < 0 )
+// {
+//     number = -number;
+// }
+int lastnumber = 0;
+if(number > 99 && number < 1000)
+{
+    lastnumber = number % 10;
+    Console.Write($"Последняя цифра числа {number} равна {lastnumber}");
+}
+else
+{
+    Console.Write("Это не трёхзначное число");
+}
