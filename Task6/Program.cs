9f8211af05c06d4a35156fe29dce65c5130f7055
Ilnur <gainutdinov86@list.ru>
2023-05-02 11:03:02 +0400

    Added Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 1)
+{
+    Console.WriteLine("Нечетное число");
+}
+else
+{
+    Console.WriteLine("Четное число");
+}
