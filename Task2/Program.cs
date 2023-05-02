5ed180314ae69dec6e6806fc702beeb179af3a80
Ilnur <gainutdinov86@list.ru>
2023-05-02 08:59:59 +0400

    added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine($"max = {numberA}");
+}
+else
+{
+    Console.WriteLine($"max = {numberB}");
+}
