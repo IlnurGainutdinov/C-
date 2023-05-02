d505f25266d9bac9589977d2936cdbaabe47a01c
Ilnur <gainutdinov86@list.ru>
2023-05-02 09:35:46 +0400

    Added Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB && numberA > numberC)
+{
+    Console.WriteLine($"max = {numberA}");
+}
+else if(numberB > numberA && numberB > numberC)
+{
+    Console.WriteLine($"max = {numberB}");
+}
+else 
+{
+    Console.WriteLine($"max = {numberC}");
+}
