aa27c39fefe9f44804dff2387a60a8f2bc9d4b6e
Ilnur <gainutdinov86@list.ru>
2023-05-13 18:28:53 +0400

    Added Task19Dz4

---
+++ Task19Dz4/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+// System.Console.WriteLine("Введите пятизначное число: ");
+// string number = Console.ReadLine();
+// if(number[0] == number[4] && number[1] == number[3])
+// {
+//     System.Console.WriteLine("Да");
+// }
+// else
+// {
+//     System.Console.WriteLine("Нет");
+// }
+
+
+// System.Console.WriteLine("Введите пятизначное число: ");
+// string number = Console.ReadLine();
+
+// if(number.Length < 5 || number.Length >5)
+// {
+//     System.Console.WriteLine("Это не пятизначное число");
+// }
+// else if(number[0] == number[4] && number[1] == number[3])
+// {
+//     System.Console.WriteLine("Да");
+// }
+// else
+// {
+//     System.Console.WriteLine("Нет");
+// }
+
+System.Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int i = 1;
+if(number[0] == number[number.Length - 1])
+{   
+    while(i < number.Length / 2)
+    {
+        
+        if(number[i] == number[number.Length - 1 - i])
+        {
+            i++;
+        }
+    }
+    System.Console.WriteLine("Да");
+}
+    
+else
+{
+    System.Console.WriteLine("Нет");
+}
